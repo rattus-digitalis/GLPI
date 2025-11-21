096349f2357ce6a3310df63c0a3725dd5b90b2ac
rattus-digitalis <victor.michi@coda-student.school>
2025-11-21 17:27:46 +0100

    big fix

---
+++ ansible/roles/glpi/files/plugins/inventory/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.6.2',
+        'version' => '1.6.2.0',
+        'reference' => '7059c08d93bdd5cbb03787dbe8815601331f6bfa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.6.2',
+            'version' => '1.6.2.0',
+            'reference' => '7059c08d93bdd5cbb03787dbe8815601331f6bfa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
